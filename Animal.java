7407c795448dfcd67754c38d31ebc8d78bcd5f68
Hamed Raza <hamedraza444@gmail.com>
2025-07-27 11:24:38 +0530

    Merge branch 'main' of https://github.com/HamedRaza004/Java_Projects git push -u origin main  :wq

---
+++ Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+    public void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Bark");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+}
