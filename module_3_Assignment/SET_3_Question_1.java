20590f96373848d720115d14303860e1f2be953a
Hamed Raza <hamedraza444@gmail.com>
2025-09-01 22:03:05 +0530

    Added SET_3 Question 1 to 5

---
+++ module_3_Assignment/SET_3_Question_1.java
@@ -0,0 +1,15 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class SET_3_Question_1 {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("input.txt")) {
+            int i;
+            while ((i = fis.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
